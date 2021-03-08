aa78432d3c0b62a98511eec87ff27a54cd6f4458
Scott Strittmatter <scott.a.strittmatter@gmail.com>
2021-03-08 15:35:52 -0600

    made scheduleStyling function to style the calendar rows based on the time of day it is.

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,66 @@
-// Create variable with today's date, format it, and add it as text to the id referenced in the HTML
-let todaysDate = moment().format("dddd, MMMM, Do");
-$("#currentDay").text(todaysDate);
+let today = moment(); // Current day's date
+let currentHour = moment().format("HH"); // Current day's time
 
-let currentTime = moment().format("h:mm:ss a");
+// let scheduleEl = document.querySelector("#schedule");
 
-let timeAtNine = moment("#9am").format("h:mm:ss a")
+$("#currentDay").text(today.format("dddd, MMMM, Do")); // Displays the current date in the header
 
-console.log(currentTime);
+today = today.format("YYYY-MM-DD"); // Changing value of Today with a new format to be able to do conditionals
 
-console.log(timeAtNine);
+
+console.log(moment(`${today} 9:00:00`).isBefore(`${today} ${currentHour}`));
+
+
+function scheduleStyling() {
+    document.querySelectorAll("#schedule").forEach(element => {
+
+        if (moment(`${today} ${element.dataset.time}`).isBefore(`${today} ${currentHour}`)) {
+            element.textContent = "This time already passed";
+            element.setAttribute("class", "col-lg-10 col-8 p-4 past")
+        } else if (moment(`${today} ${element.dataset.time}`).isAfter(`${today} ${currentHour}`)){
+            element.textContent = "This is the future";
+            element.setAttribute("class", "col-lg-10 col-8 p-4 future")
+        } else {
+            element.textContent = "Current hour";
+            element.setAttribute("class", "col-lg-10 col-8 p-4 present")
+        }
+
+    })
+}
+    scheduleStyling();
+
+
+
+
+// let workHours = [
+//     "9:00",
+//     "10:00",
+//     "11:00",
+//     "12:00",
+//     "13:00",
+//     "14:00",
+//     "15:00",
+//     "16:00",
+//     "17:00"
+// ]
+
+// console.log(moment(`${today} ${workHours[0]}`).isBefore(`${today} ${currentHour}`));
+
+// function scheduleStyling() {
+//     for (let i = 0; i < workHours.length; i++) {
+
+//         if (moment(`${today} ${workHours[i]}`).isBefore(`${today} ${currentHour}`)) {
+//             console.log("The time has passed");
+//             document.querySelectorAll("#schedule").forEach(element =>{
+//                 element.textContent = "hey";
+//             })
+
+//         } else if (moment(`${today} ${workHours[i]}`).isAfter(`${today} ${currentHour}`)) {
+//             console.log("This time is in the future");
+//         } else {
+//             console.log(`We are in the ${currentHour}th hour currently`);
+//         }
+//     }
+// }
+
+// scheduleStyling();
