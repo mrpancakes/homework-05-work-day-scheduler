29ae41b46dd10670cc6f2e78b7f2ab0fdf7be3a3
Scott Strittmatter <scott.a.strittmatter@gmail.com>
2021-03-10 12:13:16 -0600

    Created displayEntries() function and looped through localStorage to display the calendar entries on the page.

--- assets/main.js
+++ assets/main.js
@@ -2,15 +2,37 @@ let today = moment(); // Current day's date
 let currentHour = moment().hours(); // Current day's time
 $("#currentDay").text(today.format("dddd, MMMM, Do")); // Displays the current date in the header
 
+// $("#9").val(localStorage.getItem("9")); // displays just the 9am hour. Try to loop this
+
+$(".saveBtn").click(function () {
+    let btn = $(this);
+    let textarea = btn.siblings("textarea");   // grab the sibling of the button with the tag name of textarea
+    let textareaValue = textarea.val();  // jquery method of getting the value of our textarea;
+    let time = textarea.attr("id");
+    localStorage.setItem(time, textareaValue);
+});
+
+function displayEntries() {
+    const localStorageKeys = Object.entries(localStorage);
+    for (let i = 0; i < localStorageKeys.length; i++) {
+        const info = localStorageKeys[i];
+        console.log(info);
+        const id = info[0];
+        const value = info[1];
+        console.log(id, value);
+        $(`#${id}`).val(value);
+    }
+  }
+
 
 $("textarea").each(function () {
-    let  hourBlock = $(this).attr("id");
+    let hourBlock = $(this).attr("id");
 
-    if (hourBlock < currentHour){
+    if (hourBlock < currentHour) {
         $(this).addClass("past");
         $(this).removeClass("present");
         $(this).removeClass("future");
-    } else if (hourBlock > currentHour){
+    } else if (hourBlock > currentHour) {
         $(this).addClass("future");
         $(this).removeClass("past");
         $(this).removeClass("present");
@@ -22,4 +44,4 @@ $("textarea").each(function () {
 })
 
 
-displayEntries();
+ displayEntries();
